511558fa558960b157fd8e2335f937f3d86fa40f
Dzmitry Kishylau <dzmitry_kishylau@Dzmitrys-MacBook-Pro.local>
2022-04-30 07:54:49 -0700

    Use docker versions of jq and yq; wrap octavia docker command in function

--- configure_connection.sh
+++ configure_connection.sh
@@ -5,11 +5,21 @@ mkdir -p work_files/config/sources/github_custom
 mkdir -p work_files/config/destinations/bigquery
 mkdir -p work_files/config/connections/github_to_bigquery
 
-# TODO: install jq and yq
+# Install jq and yq
+function jq() {
+    docker run -i stedolan/jq $@
+}
+function yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}
+
+echo "Testing jq:"
+cat ./work_files/create_source_definition.json | jq '.sourceDefinitionId'
+echo "Testing yq:"
+cat ./docker-compose.yml | yq '.version'
 
 # Configure octavia
 echo "OCTAVIA_ENABLE_TELEMETRY=False" > ~/.octavia
-octavia=''
 
 # First, we need to get the id of source definition
 if [ ! -f ./work_files/source_definition_id ]
@@ -31,9 +41,12 @@ cat templates/source.yaml.templ | envsubst '$SOURCE_DEFINITION_ID' > ./work_file
 cat templates/destination.yaml.templ > ./work_files/config/destinations/bigquery/configuration.yaml
 
 echo 'Running Octavia'
+function octavia() {
+    docker run -i --rm -v $(pwd)/work_files/config:/home/octavia-project --network host --env-file ~/.octavia --user $(id -u):$(id -g) airbyte/octavia-cli:0.36.4-alpha $@
+}
 # Now we need to run octavia and create/update source and destination
-docker run -i --rm -v $(pwd)/work_files/config:/home/octavia-project --network host --env-file ~/.octavia --user $(id -u):$(id -g) airbyte/octavia-cli:0.36.4-alpha apply --force --file ./sources/github_custom/configuration.yaml
-docker run -i --rm -v $(pwd)/work_files/config:/home/octavia-project --network host --env-file ~/.octavia --user $(id -u):$(id -g) airbyte/octavia-cli:0.36.4-alpha apply --force --file ./destinations/bigquery/configuration.yaml
+octavia apply --force --file ./sources/github_custom/configuration.yaml
+octavia apply --force --file ./destinations/bigquery/configuration.yaml
 
 # Extract source and destination identifiers so that we can use them in the connection file
 export SOURCE_ID=$(cat ./work_files/config/sources/github_custom/state.yaml | yq ".resource_id")
@@ -57,6 +70,6 @@ fi
 export OPERATION_ID=$(cat ./work_files/operation_id)
 
 cat templates/connection.yaml.templ | envsubst '$SOURCE_ID $DESTINATION_ID $OPERATION_ID' > ./work_files/config/connections/github_to_bigquery/configuration.yaml
-docker run -i --rm -v $(pwd)/work_files/config:/home/octavia-project --network host --env-file ~/.octavia --user $(id -u):$(id -g) airbyte/octavia-cli:0.36.4-alpha apply --force --file ./connections/github_to_bigquery/configuration.yaml
+octavia apply --force --file ./connections/github_to_bigquery/configuration.yaml
 # Save connection id in a file, so that we can use it
 cat ./work_files/config/connections/github_to_bigquery/state.yaml | yq ".resource_id" > ./work_files/connection_id
